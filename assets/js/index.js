f8d258f863c1a5323e65dffc103f7202feb8542c
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-07-08 06:41:17 +0200

    08/07/24 06:41

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,6 @@
 const nav = document.querySelector("nav");
 const openIcon = document.getElementById("open-icon");
 const closeIcon = document.getElementById("close-icon");
-//Au chargement de la page en mode responsive, le menu reste fermé
-
-function windowLoad() {
-  nav.style.display = "none";
-  openIcon.style.display = "block";
-  closeIcon.style.display = "none";
-}
 
 //Ouverture du menu si clic sur l'icône d'ouverture du menu
 function openNav() {
